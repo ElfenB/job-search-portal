ea7f32da7c23fb259dc80d261d82e9cc3166c397
Benjamin Elfen <benjamin@elfen-online.de>
2024-03-10 21:27:34 +0100

    feat: add i18n support for translations

--- src/pages/Overview.tsx
+++ src/pages/Overview.tsx
@@ -2,22 +2,24 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/rea
 import { OfferList } from "../components/OfferList";
 import { useDarkMode } from "../utils/useDarkMode";
 import { offerListMockData } from "../components/OfferList.mockData";
+import { useTranslation } from "react-i18next";
 
 export function Overview() {
+  const { t } = useTranslation();
   const { darkMode } = useDarkMode();
 
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Overview</IonTitle>
+          <IonTitle>{t("label.overview")}</IonTitle>
         </IonToolbar>
       </IonHeader>
 
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Overview</IonTitle>
+            <IonTitle size="large">{t("label.overview")}</IonTitle>
 
             <img
               src={darkMode ? "/jobber-dark.svg" : "/jobber.svg"}
