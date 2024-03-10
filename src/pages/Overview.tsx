2d0a5eb85f0f873a1a6b011823d746dbf841ec91
Benjamin Elfen <benjamin@elfen-online.de>
2024-03-10 20:52:55 +0100

    feat: add dark mode support to Overview component using useDarkMode hook
    The useDarkMode hook is added to the Overview component to enable toggling
    between light and dark mode. This hook listens for changes in the user's
    preferred color scheme using matchMedia and adjusts the darkMode state
    accordingly. Additionally, the favicon is updated to a dark mode variant.
    
    The Xcode project file is also updated to include dark mode image assets for
    AppIcon in different sizes to support dark mode appearance on iOS devices.

--- src/pages/Overview.tsx
+++ src/pages/Overview.tsx
@@ -1,13 +1,22 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
 import { OfferList } from "../components/OfferList";
+import { useDarkMode } from "../utils/useDarkMode";
 
 export function Overview() {
+  const { darkMode } = useDarkMode();
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonTitle>Overview</IonTitle>
         </IonToolbar>
+
+        <img
+          style={{ position: "absolute", height: "3rem", top: "1rem", right: "1rem" }}
+          src={darkMode ? "/jobber-dark.svg" : "/jobber.svg"}
+          alt="Jobber logo"
+        />
       </IonHeader>
 
       <IonContent fullscreen>
