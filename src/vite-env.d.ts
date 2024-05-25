7e067cf0f40b51d78882e9bb2bb19edc9b36d743
Benjamin Elfen <benjamin@elfen-online.de>
2024-05-25 15:06:50 +0200

    fix: added backend url for native

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,5 +6,6 @@ interface ImportMetaEnv {
   readonly VITE_AUTH0_CLIENT_ID: string;
   readonly VITE_AUTH0_CLIENT_ID_WEB: string;
   readonly VITE_AUTH0_DOMAIN: string;
+  readonly VITE_BACKEND_URL?: string;
   // more env variables...
 }
