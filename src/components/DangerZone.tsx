3ffb4cbd7cbbe3f234a314f552f2b5358cea18c3
Benjamin Elfen <benjamin@elfen-online.de>
2024-05-29 18:23:13 +0200

    fix: only author can see delete button

---
+++ src/components/DangerZone.tsx
@@ -0,0 +1,48 @@
+import { IonAlert, IonButton, IonIcon, IonItemDivider, IonLabel, IonText } from '@ionic/react';
+import { trash } from 'ionicons/icons';
+import { useTranslation } from 'react-i18next';
+
+type Props = {
+  offerType: string;
+  onDelete: () => void;
+};
+
+export function DangerZone({ offerType, onDelete }: Props) {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <IonItemDivider color="danger">
+        <IonLabel>{t('label.dangerZone')}</IonLabel>
+      </IonItemDivider>
+
+      {/* ID triggers IonAlert */}
+      <div style={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
+        <IonButton className="ion-padding" color="danger" id="present-alert">
+          <IonIcon icon={trash} />
+          <IonText style={{ marginLeft: '.5rem' }}>
+            <span>
+              {t('label.deleteSomething')} {t(offerType)}
+            </span>
+          </IonText>
+        </IonButton>
+      </div>
+
+      {/* Is triggered by id from IonButton */}
+      <IonAlert
+        buttons={[
+          { role: 'cancel', text: t('label.cancel') },
+          {
+            handler: () => {
+              onDelete();
+            },
+            role: 'confirm',
+            text: t('label.delete'),
+          },
+        ]}
+        header={t('label.deleteOfferConfirmation')}
+        trigger="present-alert"
+      />
+    </>
+  );
+}
--- src/components/OfferDetailsView.tsx
+++ src/components/OfferDetailsView.tsx
@@ -1,12 +1,14 @@
-import { useCallback, useState } from "react";
-import { IonAlert, IonButton, IonIcon, IonItemDivider, IonLabel, IonText } from "@ionic/react";
-import type { Offer } from "@prisma/client";
-import { calendarOutline, eye, locationOutline, trash } from "ionicons/icons";
-import { useTranslation } from "react-i18next";
-import { Divider } from "./Divider";
-import { InfoItem } from "./InfoItem";
-import { OfferDetailsSection } from "./OfferDetailsSection";
-import { UserAvatar } from "./UserAvatar";
+import { useCallback, useState } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { IonText } from '@ionic/react';
+import type { Offer } from '@prisma/client';
+import { calendarOutline, eye, locationOutline } from 'ionicons/icons';
+import { useTranslation } from 'react-i18next';
+import { DangerZone } from './DangerZone';
+import { Divider } from './Divider';
+import { InfoItem } from './InfoItem';
+import { OfferDetailsSection } from './OfferDetailsSection';
+import { UserAvatar } from './UserAvatar';
 
 type Props = {
   offer: Offer;
@@ -15,21 +17,22 @@ type Props = {
 
 export function OfferDetailsView({ offer, onDelete }: Props) {
   const { t } = useTranslation();
+  const { user } = useAuth0();
 
   const { authorId, createdAt, currency, description, image, location, money, offerType, paymentType, title, views } =
     offer;
 
-  const [imageHeight, setImageHeight] = useState<"25vh" | undefined>("25vh");
+  const [imageHeight, setImageHeight] = useState<'25vh' | undefined>('25vh');
 
   const toggleImageHeight = useCallback(() => {
-    setImageHeight(imageHeight === "25vh" ? undefined : "25vh");
+    setImageHeight(imageHeight === '25vh' ? undefined : '25vh');
   }, [imageHeight, setImageHeight]);
 
   return (
     <>
       {image && (
         <div role="button" tabIndex={0} onClick={toggleImageHeight} onKeyDown={toggleImageHeight}>
-          <img alt={title} src={image} style={{ height: imageHeight, objectFit: "cover", width: "100%" }} />
+          <img alt={title} src={image} style={{ height: imageHeight, objectFit: 'cover', width: '100%' }} />
         </div>
       )}
 
@@ -41,17 +44,17 @@ export function OfferDetailsView({ offer, onDelete }: Props) {
         {money === null ? (
           <></>
         ) : (
-          <IonText color="primary" style={{ fontWeight: "bold" }}>
+          <IonText color="primary" style={{ fontWeight: 'bold' }}>
             <p>
-              {money} {currency} ({t(paymentType ?? "label.paymentTypeNotSpecified")})
+              {money} {currency} ({t(paymentType ?? 'label.paymentTypeNotSpecified')})
             </p>
           </IonText>
         )}
 
         <InfoItem icon={locationOutline}>{location}</InfoItem>
-        <div style={{ display: "flex", marginTop: "1rem" }}>
+        <div style={{ display: 'flex', marginTop: '1rem' }}>
           <InfoItem icon={calendarOutline}>{createdAt.toLocaleDateString()}</InfoItem>
-          <InfoItem icon={eye} style={{ marginLeft: "1rem" }}>
+          <InfoItem icon={eye} style={{ marginLeft: '1rem' }}>
             {String(views)}
           </InfoItem>
         </div>
@@ -60,11 +63,11 @@ export function OfferDetailsView({ offer, onDelete }: Props) {
       <Divider />
 
       <OfferDetailsSection>
-        {title === "" ? (
+        {title === '' ? (
           <></>
         ) : (
           <div>
-            <h3>{t("label.offerdetails")}</h3>
+            <h3>{t('label.offerdetails')}</h3>
             <p>{description}</p>
           </div>
         )}
@@ -76,39 +79,12 @@ export function OfferDetailsView({ offer, onDelete }: Props) {
         <UserAvatar userId={authorId} withName />
       </OfferDetailsSection>
 
-      <Divider />
-
-      <IonItemDivider color="danger">
-        <IonLabel>{t("label.dangerZone")}</IonLabel>
-      </IonItemDivider>
-
-      {/* ID triggers IonAlert */}
-      <div style={{ display: "flex", justifyContent: "center", width: "100%" }}>
-        <IonButton className="ion-padding" color="danger" id="present-alert">
-          <IonIcon icon={trash} />
-          <IonText style={{ marginLeft: ".5rem" }}>
-            <span>
-              {t("label.deleteSomething")} {t(offerType)}
-            </span>
-          </IonText>
-        </IonButton>
-      </div>
-
-      {/* Is triggered by id from IonButton */}
-      <IonAlert
-        buttons={[
-          { role: "cancel", text: t("label.cancel") },
-          {
-            handler: () => {
-              onDelete();
-            },
-            role: "confirm",
-            text: t("label.delete"),
-          },
-        ]}
-        header={t("label.deleteOfferConfirmation")}
-        trigger="present-alert"
-      />
+      {user?.sub === authorId && (
+        <>
+          <Divider />
+          <DangerZone offerType={offerType} onDelete={onDelete} />
+        </>
+      )}
     </>
   );
 }
