3d81d4c294824393c51d211d6383e122d5d891c7
Benjamin Elfen <benjamin@elfen-online.de>
2024-07-05 10:52:27 +0200

    feat: added basic pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import legacy from '@vitejs/plugin-legacy';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
     'import.meta.env.VITE_APP_VERSION': JSON.stringify(process.env.npm_package_version),
   },
-  plugins: [react(), legacy()],
+  plugins: [react(), legacy(), VitePWA({
+    devOptions: {
+      enabled: true
+    }, registerType: 'autoUpdate'
+  })],
 });
